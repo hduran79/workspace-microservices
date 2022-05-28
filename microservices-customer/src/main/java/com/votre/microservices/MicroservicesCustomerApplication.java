de7d59d63187421ef3ccc0c42f2afa840b082c14
hduran <hugoivandr@gmail.com>
2022-05-28 08:44:38 -0500

    Add project eureka-server

--- microservices-customer/src/main/java/com/votre/microservices/MicroservicesCustomerApplication.java
+++ microservices-customer/src/main/java/com/votre/microservices/MicroservicesCustomerApplication.java
@@ -2,8 +2,10 @@ package com.votre.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesCustomerApplication {
 	
 	public static void main(String[] args) {
--- microservices-shopping/src/main/java/com/votre/microservices/shopping/MicroservicesShoppingApplication.java
+++ microservices-shopping/src/main/java/com/votre/microservices/shopping/MicroservicesShoppingApplication.java
@@ -2,8 +2,10 @@ package com.votre.microservices.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesShoppingApplication {
 
 	public static void main(String[] args) {
